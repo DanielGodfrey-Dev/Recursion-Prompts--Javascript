7a9adc256cd571438add6889cd45235c8d36d110
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-01-25 18:25:59 -0800

    Solved = 2
    factorial & sum of integers

--- src/recursion.js
+++ src/recursion.js
@@ -7,16 +7,46 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
-};
+	if (n < 0) {
+		return null;
+	}
+
+	if (n === 0) {
+		return 1;
+	}
+
+	return (n * factorial(n-1));
+
+}
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+	if (array.length === 0) { //note: this is correct way to express an empty array, not 'array === []'
+		return 0;
+	}
+
+	if (array.length === 1) {
+		return array[0];
+	}
+
+	return array[array.length - 1] + sum(array.slice(0, (array.length - 1)));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+	if (array.length === 0) { //note: this is correct way to express an empty array, not 'array === []'
+		return 0;
+	}
+
+	if (array.length === 1) {
+		return array[0];
+	}
+
+	array = Array.prototype.concat.apply([], array); //appears to work with this flattening method in repl.it but...:(
+
+	return array[array.length - 1] + arraySum(array.slice(0, (array.length - 1)));
 };
 
 // 4. Check if a number is even.
